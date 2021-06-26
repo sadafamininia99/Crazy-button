74c821b6e2c7f4100dc574b599ff6da3df870063
= <aminicomputer96@gmail.com>
2021-06-26 10:30:15 +0430

    i have error

--- script.js
+++ script.js
@@ -2,9 +2,27 @@
 
 const crzaybutton =document.querySelector('.btn-crazy');
 
-//define our fun
+//define our function
+function goCrazy()
+{
+    //get a random num 4 left offset
+    //get a number for top offset
 
+    const offsetLeft =Math.random();
+    const offsetTop =Math.random();
+
+    //test 
+    console.log(offsetLeft ,offsetTop);
+
+    //apply those numbers to the button
+
+    crzaybutton.style.top =offsetTop;
+    crzaybutton.style.left=offsetLeft;
+
+}
 
 
 //add event listiner
-crzaybutton.addEventListener('')
+window.onunload.function();{;
+   crzaybutton.addEventListener('mouseenter',goCrazy) 
+};
